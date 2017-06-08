2d593a1d2bb2ea51d2740359b63e9c1d6d477edb
Dominik Alberski <dominik.alberski@gmail.com>
2017-06-08 17:27:22 +0200

    add test for edit action

--- test/controllers/alcoholes_controller_test.rb
+++ test/controllers/alcoholes_controller_test.rb
@@ -1,36 +1,43 @@
 require 'test_helper'
 
-
+#
 class AlcoholesControllerTest < ActionDispatch::IntegrationTest
-
-  test "should get show response success" do
+  test 'should get show response success' do
     sample = create(:alcohol)
     get alcohol_path(sample.id)
     assert_response :success
   end
 
-  test "should get show 1 alcohol" do
+  test 'should get show 1 alcohol' do
     sample = create(:alcohol)
     get alcohol_path(sample.id)
-    assert_select 'p', sample.name
+    assert_select '.name', sample.name
   end
 
-  test "should get new response success" do
+  test 'should get new response success' do
     sign_in create(:user)
     get new_alcohol_path
     assert_response :success
   end
 
-  test "user shuld be able to create alcohol" do
+  test 'user shuld be able to create alcohol' do
     sign_in create(:user)
-    alcohol = { alcohol: {name: 'sample 1', brand: 'sample brand', kind: 'wine' }}
+    alcohol = { alcohol: { name: 'sample 1', brand: 'sample brand', kind: 'wine' } }
     post alcohols_path alcohol
-    assert_response :redirect 
-    assert_not_nil(Alcohol.find_by name: 'sample 1', kind: "wine")
+    assert_response :redirect
+    assert_not_nil(Alcohol.find_by(name: 'sample 1', kind: 'wine'))
   end
 
+  test 'user shuld be able to create alcohol(wine) with proper details' do
+    sign_in create(:user)
+    alcohol = { alcohol: { name: 'sample 1', brand: 'sample brand', kind: 'wine', wine_attributes: { kind: 'semi-dry', color: 'red' } } }
+    post alcohols_path alcohol
+    assert_response :redirect
+    assert_not_nil((Alcohol.find_by name: 'sample 1', kind: 'wine').wine)
+    assert_equal('semi-dry', Alcohol.find_by(name: 'sample 1', kind: 'wine').wine.kind)
+  end
 
-  test "sign_in user shuld not be able to delete other user alcohol and set proper flash msg." do
+  test 'sign_in user shuld not be able to delete other user alcohol and set proper flash msg.' do
     other_user_alcohol = create(:alcohol)
     sign_in create(:user)
     delete alcohol_path(other_user_alcohol.id)
@@ -41,12 +48,31 @@ class AlcoholesControllerTest < ActionDispatch::IntegrationTest
 
   test 'sign_in user shuld be able to delete assign alcohol' do
     alcohol = create(:alcohol)
-    sign_in alcohol.user
+    sign_in alcohol.user  
     delete alcohol_path(alcohol.id)
     assert_response :redirect
-    assert_raises( "ActiveRecord::RecordNotFound" ) { Alcohol.find(alcohol.id) }
+    assert_raises('ActiveRecord::RecordNotFound') { Alcohol.find(alcohol.id) }
     assert_equal("Successfully deleted alcohol #{alcohol.name}", flash[:notice])
   end
 
+  test 'get edit shuld response success' do
+    alcohol = create(:alcohol)
+    sign_in alcohol.user
+    get edit_alcohol_path(alcohol)
+    assert_response :success
+  end
 
+  test 'Shuld get show have link to edit' do
+    alcohol = create(:alcohol)
+    sign_in alcohol.user
+    get alcohol_path(alcohol)
+    assert_select 'a', alcohol.name
+  end
+
+  test 'edit shuld show proper form with valius' do
+    alcohol = create(:alcohol)
+    sign_in alcohol.user
+    get edit_alcohol_path(alcohol)
+    assert_select 'label', 'Name'
+  end
 end
