6e2cebbb34b1974033a7da823cca8950d4654130
Dominik Alberski <dominik.alberski@gmail.com>
2017-06-08 17:56:04 +0200

    cosmetic changes to code

--- test/factories/alcohols.rb
+++ test/factories/alcohols.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :alcohol do
-    name "Sample Alcohol Name"
-    brand "Sample Alcohol Brand"
-    kind "vodka"
-    country "Poland"
+    name 'Sample Alcohol Name'
+    brand 'Sample Alcohol Brand'
+    kind 'vodka'
+    country 'Poland'
     alk 40
     price 40
     association :user, factory: :user
